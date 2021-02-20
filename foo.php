82f6555883bd9b4ae1f9b072b93633c113ee0f48
m4medbekov <m4medbekov@gmail.com>
2021-02-20 15:50:09 +0300

    add update

--- foo.php
+++ foo.php
@@ -21,3 +21,14 @@ if (isset($_POST['add'])) {
 $sql = $pdo->prepare("SELECT * FROM users_1");
 $sql->execute();
 $result = $sql->fetchAll(PDO::FETCH_OBJ);
+
+// Update
+
+if (isset($_POST['edit'])) {
+    $sql = ("UPDATE users_1 SET name=?, email=? WHERE id=?");
+    $query = $pdo->prepare($sql);
+    $query->execute([$name, $email, $get_id]);
+    if ($query) {
+        header("Location: ". $_SERVER['HTTP_REFERER']);
+    }
+}
--- index.php
+++ index.php
@@ -21,7 +21,8 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <button class="btn btn-success mt-2" data-toggle="modal" data-target="#create"><i class="fa fa-plus"></i></button>
+        <button class="btn btn-success mt-2" data-toggle="modal" data-target="#create"><i
+            class="fa fa-plus"></i></button>
         <table class="table table-striped table-hover mt-2">
           <thead class="thead-dark">
             <th>ID</th>
@@ -36,10 +37,38 @@
               <td><?php echo $res->name; ?></td>
               <td><?php echo $res->email; ?></td>
               <td>
-                <a href="" class="btn btn-success"><i class="fa fa-edit"></i></a>
+                <a href="?id=<?php echo $res->id; ?>" class="btn btn-success" data-toggle="modal" data-target="#edit<?php echo $res->id; ?>"><i class="fa fa-edit"></i></a>
                 <a href="" class="btn btn-danger"><i class="fa fa-trash-alt"></i></a>
               </td>
             </tr>
+            <!-- Modal edit -->
+            <div class="modal fade" id="edit<?php echo $res->id; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Изменить запись</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <form action="" method="post">
+                      <div class="form-group">
+                        <small>Имя</small>
+                        <input type="text" class="form-control" name="name"  value="<?php echo $res->name; ?>">
+                      </div>
+                      <div class="form-group">
+                        <small>Email</small>
+                        <input type="text" class="form-control" name="email" value="<?php echo $res->email; ?>" >
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                    <button type="submit" class="btn btn-primary" name="edit">Изменить</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Modal edit -->
             <?php } ?>
           </tbody>
         </table>
